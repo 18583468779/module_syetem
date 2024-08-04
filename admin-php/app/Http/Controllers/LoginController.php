5e787b0ca468d791b6c87d25117acdd193b93df8
xiewen <1123654054@qq.com>
2024-08-04 16:36:29 +0800

    user表结构修改

--- admin-php/app/Http/Controllers/LoginController.php
+++ admin-php/app/Http/Controllers/LoginController.php
@@ -9,29 +9,27 @@ use Illuminate\Validation\ValidationException;
 
 class LoginController extends Controller
 {
-    //
     public function __invoke(Request $request)
     {
         $request->validate([
             'email' => ['required', 'email'],
             'password' => ['required', 'min:3'],
         ]);
-
         $user = User::where('email', $request->email)->first();
-
         if (!$user) {
             throw ValidationException::withMessages([
                 'email' => '用户不存在'
             ]);
         }
-
         if (!Hash::check($request->password, $user->password)) {
             throw ValidationException::withMessages([
                 'password' => '密码输入错误'
             ]);
         }
-
-
-        return ['token' => $user->createToken('auth')->plainTextToken];
+        return [
+            'token' => $user->createToken('auth')->plainTextToken,
+            'flag' => 0,
+            'user' => $user
+        ];
     }
 }
