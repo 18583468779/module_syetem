e7a80d42fdba8a5e013a87efde060a5077831091
xiewen <1123654054@qq.com>
2024-08-07 21:41:40 +0800

    修改smtp协议

--- admin-php/app/Http/Controllers/ValidateCodeController.php
+++ admin-php/app/Http/Controllers/ValidateCodeController.php
@@ -3,12 +3,16 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\ValidateCodeRequest;
+use App\Models\User;
+use App\Notifications\EmailValidateCodeNotification;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Notification;
 
 class ValidateCodeController extends Controller
 {
     //
     public function guest(ValidateCodeRequest $request)
     {
+        Notification::send(User::factory()->make(['email' => $request->account]), new EmailValidateCodeNotification(3124));
     }
 }
