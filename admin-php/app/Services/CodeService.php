95088988a41a0e772e7b229432b7741a2c0bd06d
xiewen <1123654054@qq.com>
2024-08-08 19:50:06 +0800

    update

--- admin-php/app/Services/CodeService.php
+++ admin-php/app/Services/CodeService.php
@@ -5,6 +5,7 @@ namespace App\Services;
 use Illuminate\Support\Facades\Notification;
 use App\Models\User;
 use App\Notifications\EmailValidateCodeNotification;
+use Illuminate\Support\Facades\Cache;
 
 class CodeService
 {
@@ -13,12 +14,15 @@ class CodeService
     {
         $this->account = $account;
         $action = filter_var($account, FILTER_VALIDATE_EMAIL) ? 'email' : 'mobile';
+        if (Cache::get($this->account)) abort(403, '验证码已发送');
         $this->$action();
     }
 
     protected function email()
     {
-        Notification::send(User::factory()->make(['email' => $this->account]), new EmailValidateCodeNotification(3124));
+        $code = mt_rand(1000, 9999);
+        Cache::put($this->account, $code);
+        Notification::send(User::factory()->make(['email' => $this->account]), new EmailValidateCodeNotification($code));
     }
 
     protected function mobile()
